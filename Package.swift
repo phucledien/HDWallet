0305545fac0a4944a4e945f205fd1c302cb47a43
phucledien <phucledien@gmail.com>
2021-07-17 01:59:26 +0700

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -13,11 +13,17 @@ let package = Package(
             targets: ["HDWalletKit"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.1"),
     ],
     targets: [
         .target(
             name: "HDWalletKit",
-            dependencies: []),
+            dependencies: [
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+                .product(name: "secp256k1", package: "secp256k1"),
+            ],
+            path: "HDWalletKit"),
         .testTarget(
             name: "HDWalletKit_Tests",
             dependencies: []),
